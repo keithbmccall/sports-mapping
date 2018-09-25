cfc436d1b4ab9c16219d293a2d22eca28016f872
Keith McCall <keithmccall@ny-0146-aa.acada.ca>
2018-09-25 17:42:07 -0400

    quick update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import { MapComponent } from './shared/components/map/map.component';
+import { LayoutComponent } from './shared/containers/layout/layout.component';
+import { LeftPanelComponent } from './shared/containers/left-panel/left-panel.component';
+import { RightPanelComponent } from './shared/containers/right-panel/right-panel.component';
+import { CentralPanelComponent } from './shared/containers/central-panel/central-panel.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    LayoutComponent,
+    LeftPanelComponent,
+    RightPanelComponent,
+    CentralPanelComponent
+
   ],
   imports: [
     BrowserModule
@@ -13,4 +25,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
