aab72e2fa9acaec1bfb12a87d03165846f3450d9
Keith McCall <keithmccall@ny-0146-aa.acada.ca>
2018-10-02 16:57:27 -0400

    quick update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,6 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import { MapComponent } from './shared/components/map/map.component';
 import { LayoutComponent } from './shared/containers/layout/layout.component';
-import { LeftPanelComponent } from './shared/containers/left-panel/left-panel.component';
-import { RightPanelComponent } from './shared/containers/right-panel/right-panel.component';
-import { CentralPanelComponent } from './shared/containers/central-panel/central-panel.component';
 import { PanelRightComponent } from './shared/containers/panel-right/panel-right.component';
 import { PanelLeftComponent } from './shared/containers/panel-left/panel-left.component';
 import { PanelMainComponent } from './shared/containers/panel-main/panel-main.component';
@@ -20,8 +17,6 @@ import { PanelMainComponent } from './shared/containers/panel-main/panel-main.co
     PanelRightComponent,
     PanelLeftComponent,
     PanelMainComponent
-
-
   ],
   imports: [
     BrowserModule
