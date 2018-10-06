e3e6bfe00c5c86fae34164ba3a7298469dcee59b
keithmccall <keithcodes@gmail.com>
2018-10-06 03:50:41 -0400

    quick update

--- src/app/data/nfl-teams.ts
+++ src/app/data/nfl-teams.ts
@@ -1,8 +1,12 @@
 import { Team, Division, Conference, league } from "./team-data";
 
-export const TEAMS: Team[] = [];
-export const DIVISIONS: Division[] = league.conferences.map(
-  conference => conference.divisions
-);
-
 export const CONFERENCES: Conference[] = league.conferences;
+export const DIVISIONS: Division[] = league.conferences.reduce((a, c) => {
+  console.log(a,c)
+  a.push(...c.divisions);
+  return a;
+}, []);
+export const TEAMS: Team[] = DIVISIONS.reduce((a, c) => {
+  a.push(...c.teams);
+  return a;
+}, []);
