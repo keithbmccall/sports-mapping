ff2347ad410f2fcdc9c569242fdd77abeb3e6441
keithmccall <keithcodes@gmail.com>
2018-10-07 21:38:46 -0400

    quick update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,22 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
-import { MapComponent } from "./shared/components/map/map.component";
 import { LayoutComponent } from "./shared/containers/layout/layout.component";
 import { PanelSideComponent } from './shared/containers/panels/panel-side/panel-side.component';
 import { PanelCenterComponent } from './shared/containers/panels/panel-center/panel-center.component';
 import { PanelOptionsComponent } from './shared/containers/panels/panel-options/panel-options.component';
 import { ListLinksComponent } from './shared/components/tools/lists/list-links/list-links.component';
+import { MapComponent } from './shared/components/maps/map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent,
     LayoutComponent,
     PanelSideComponent,
     PanelCenterComponent,
     PanelOptionsComponent,
-    ListLinksComponent
+    ListLinksComponent,
+    MapComponent
   ],
   imports: [BrowserModule],
   providers: [],
